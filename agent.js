8db802c3773495cfa6e06048088a70920921c70e
Aditya Narayan Rai <anrai0410@gmail.com>
2025-11-01 03:19:35 +0000

    configure agentic workflow

---
+++ agent.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+require('dotenv').config();
+
+async function updateNaukri() {
+  const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+      await page.goto('https://www.naukri.com/mnjuser/profile');
+
+        await page.type('#usernameField', process.env.NAUKRI_USER);
+          await page.type('#passwordField', process.env.NAUKRI_PASS);
+            await page.click('#loginButton');
+              await page.waitForNavigation();
+
+                await page.goto('https://www.naukri.com/mnjuser/profile/edit');
+                  await page.type('#resumeHeadline', 'Updated at ' + new Date().toISOString());
+                    await page.click('#saveButton');
+
+                      await browser.close();
+                      }
+
+                      updateNaukri();
