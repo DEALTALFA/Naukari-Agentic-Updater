74755fea2412ea4a415a80a87d77c3d92849dbb0
Aditya Narayan Rai <anrai0410@gmail.com>
2025-11-01 16:46:03 +0000

    v5.331

--- agent.js
+++ agent.js
@@ -8,7 +8,7 @@ async function updateNaukri() {
 
  await page.goto('https://www.naukri.com/nlogin/login', { waitUntil: 'domcontentloaded' });
  await page.waitForTimeout(3000);
- await page.screenshot({path:"01.png"});
+ await page.screenshot({path: "01.png"});
 //  await page.waitForSelector("//*[@id='usernameField']", { timeout: 60000 });
 //  await page.fill("//*[@id='usernameField']", process.env.NAUKRI_USER);
 //  await page.screenshot({path:"02.png"});
