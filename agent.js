e559b7868d14bb4b22bc24f4b745d7df8d813d09
Aditya Narayan Rai <anrai0410@gmail.com>
2025-11-01 15:12:07 +0000

    v5.1

--- agent.js
+++ agent.js
@@ -5,14 +5,16 @@ async function updateNaukri() {
   const page = await browser.newPage();
 
   await page.goto('https://www.naukri.com/mnjuser/profile');
-
-  await page.waitForSelector('#resumeHeadline');
-  await page.click('#resumeHeadline');
-
-  await page.waitForSelector('#resumeHeadlineTxt');
-  await page.fill('#resumeHeadlineTxt', 'passionForField');
-
+  await page.fill('//*[@id="usernameField"]',  process.env.NAUKRI_USER);
+  await page.fill('//*[@id="passwordField"]',  process.env.NAUKRI_PASS);
   await page.click('#saveHeadline');
+
+ await page.click('#loginButton');
+  page.waitForSelector;
+
+                await page.goto('https://www.naukri.com/mnjuser/profile/edit');
+                  await page.fill('#resumeHeadline', 'Updated at ' + new Date().toISOString());
+                    await page.click('#saveButton');
 
   await browser.close();
 }
