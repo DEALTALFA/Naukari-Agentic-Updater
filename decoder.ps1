4edf5c746ea4de411cc0c2894f3a58c93d985c51
Deepak Yadav <60976631+DEALTALFA@users.noreply.github.com>
2026-01-24 18:50:42 +0530

    Add progress messages to decoder script
    Added status messages to indicate progress during decoding.

--- decoder.ps1
+++ decoder.ps1
@@ -2,13 +2,14 @@
 $inputFile = "encode.json"
 $outputFile = "cookies.json"
 
+Write-Host "Starting script for Decoding"
+
 # 1. Read the Base64 string from the input file as a single string (using -Raw)
 $base64String = Get-Content -Path $inputFile -Raw
-
+Write-Host "File Read Done"
 # 2. Convert the Base64 string to a byte array
 $bytes = [Convert]::FromBase64String($base64String)
-
+Write-Host "COverting Base64 to Byte"
 # 3. Write the byte array to the output file
 [System.IO.File]::WriteAllBytes($outputFile, $bytes)
-
-echo "print"
+Write-Host "Decoding Complete"
