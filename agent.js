67f4c777a884386a19fdd5a7b474d88d8cba4d80
Deepak Yadav <alfo7742@gmail.com>
2026-08-07 17:32:30 +0530

    take screenshot of exp salary

--- agent.js
+++ agent.js
@@ -77,22 +77,26 @@ await injectCookies(context);
   }
 }
 await page.reload();
- await page.screenshot({path: "02.png"});
-    await page.waitForTimeout(10000);
+await page.screenshot({path: "02.png"});
+await page.waitForTimeout(10000);
 await page.screenshot({path: "03.png"});
-
+await page.waitForTimeout(10000);
 // await page.click('div.view-profile-wrapper > a');
-   await page.click('//a[@href="/mnjuser/profile"]');
+await page.click('(//span[text()="Jobs"])[1]');
+await page.waitForTimeout(3000);
+await page.screenshot({path: "04.png"});
+await page.click('//img[@href="https://www.naukri.com"]');
 
+await page.click('//a[@href="/mnjuser/profile"]');
  //*[text()='View']
- await page.waitForTimeout(6000);
-  await page.screenshot({path:"04.png"});
- await page.click('//*[@id="lazyResumeHead"]/div/div/div[1]/span[2]');
- await page.waitForTimeout(6000);
-  await page.screenshot({path:"05.png"});
- await page.click("//button[text()='Save']");
-  await page.waitForTimeout(6000);
-  await page.screenshot({path:"06.png"});
+await page.waitForTimeout(6000);
+await page.screenshot({path:"05.png"});
+await page.click('//*[@id="lazyResumeHead"]/div/div/div[1]/span[2]');
+await page.waitForTimeout(6000);
+await page.screenshot({path:"06.png"});
+await page.click("//button[text()='Save']");
+await page.waitForTimeout(6000);
+await page.screenshot({path:"07.png"});
  
 // await page.click('text=Login');
 //   await page.mouse.move(200, 300);
